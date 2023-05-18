0bf58ae5768fdb6eb4c9ef6f34291c0bba2e89b5
Sourav01112 <chaudharysourav997@gmail.com>
2023-05-18 20:30:44 +0530

    done

---
+++ script.js
@@ -0,0 +1,52 @@
+const weather = {
+  apikey: "98eb1f9a9a72f2becf0db8700c1dae4b",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apikey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data))
+      .catch((err) => {
+        JSAlert.alert("Please enter a valid city name");
+      });
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "http://openweathermap.org/img/wn/" + icon + ".png";
+
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+    document.querySelector(".weather").classList.remove("loading");
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Delhi");
+
+// created on 18.05.2023 by Sourav
